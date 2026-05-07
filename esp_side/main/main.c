14b27527f5b35bce310403a70729a74874736417
Dhruv Dhiman <dhruvdhiman905@gmail.com>
2026-05-07 18:24:51 +0530

    Added the gpio configuration for the motor driver

---
+++ esp_side/main/main.c
@@ -0,0 +1,24 @@
+ #include <stdio.h>
+#include "driver/gpio.h"
+
+// ======================== Space for defining the pin using macro ===========================
+
+#define M1_PIN_IN1 GPIO_NUM_18
+#define M1_PIN_IN2 GPIO_NUM_19
+#define M2_PIN_IN1 GPIO_NUM_21
+#define M2_PIN_IN2 GPIO_NUM_22
+
+// ======================= Space for writing the configuration for the pins =======================
+
+
+
+
+// ======================== Space for any function defined by the user =============================
+
+
+
+
+void app_main(void)
+{
+
+}
