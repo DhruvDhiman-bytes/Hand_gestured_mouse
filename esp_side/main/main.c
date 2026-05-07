4125ca5b1102121f87be7f026d9353f61b8a4d35
Dhruv Dhiman <dhruvdhiman905@gmail.com>
2026-05-07 18:56:23 +0530

    Added the content for the function of spi bus init

--- esp_side/main/main.c
+++ esp_side/main/main.c
@@ -6,10 +6,18 @@
 
 // ======================== Space for defining the pin using macro ===========================
 
+// pin defination for motor driver
+
 #define M1_PIN_IN1 18
 #define M1_PIN_IN2 19
 #define M2_PIN_IN1 21
 #define M2_PIN_IN2 22
+
+// pin defination for SPI communication
+
+#define MOSI_PIN 32
+#define MISO_PIN 33
+#define SPICLK_PIN 6
 
 // ======================= Space for writing the configuration for the pins =======================
 
@@ -31,6 +39,22 @@ void m2_pin_init(void) {
         .pull_up_en = GPIO_PULLUP_DISABLE,
     };
     gpio_config(&m2_config);
+}
+
+void spi_bus_init(void) {
+    spi_bus_config_t spi_bus_config = {
+        .mosi_io_num = MOSI_PIN,
+        .miso_io_num = MISO_PIN,
+        .sclk_io_num = SPICLK_PIN,
+        .data0_io_num = -1,
+        .data1_io_num = -1,
+        .data2_io_num = -1,
+        .data3_io_num = -1,
+        .data4_io_num = -1,
+        .data5_io_num = -1,
+        .data6_io_num = -1,
+        .data7_io_num = -1
+    };
 }
 
 // ======================== Space for any function defined by the user =============================
