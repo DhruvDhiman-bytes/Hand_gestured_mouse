64036ced01073203fe9dc7da546c3225af1b5113
Dhruv Dhiman <dhruvdhiman905@gmail.com>
2026-05-09 15:57:44 +0530

    Made the file for camera control and wrote the comment section of the idea behind the file and also the note to people who want to do pull request

---
+++ image_processing/camera_control.py
@@ -0,0 +1,8 @@
+# The idea behind this file is that it will do the main
+# work of image processing for the project
+# And then sent the details over to the esp32 using bluetooth
+# ======================= NOTE TO NON-AUTHOR ========================
+# If you like to make a pull request then plz do by explaining everything about your changes
+#   - The reason behind
+#   - The architecture it will support
+#   - Tools compatiblity
