4eb8caaaf1aecb9fd2e4ff2a5ef1eef10c66167d
Dhruv Dhiman <dhruvdhiman905@gmail.com>
2026-05-17 02:24:52 +0530

    Removed the static function

--- esp_side/main/wifi_control.h
+++ esp_side/main/wifi_control.h
@@ -1,6 +1,5 @@
 #ifndef WIFI_CONTROL_H_
 #define WIFI_CONTROL_H_
 
-static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void* event_data);
 esp_err_t wifi_init_sta(void);
 #endif
